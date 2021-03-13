b3c2fb64bac8fd2c7171d73e062c0b10f2a043d8
bkc-tomi <bkc.tomi1979@gamil.com>
2021-03-13 19:32:45 +0900

    ステートが変更される度にローカルストレージに反映するように変更

--- components/common/select.js
+++ components/common/select.js
@@ -4,8 +4,8 @@ export default function Select({ id="", name="", items=[] }) {
     const size = items.length;
     return (
         <select id={ id } name={ name } size={ size }>
-            { items.map(item => {
-                return <option value={ item.name }>{ item.name }</option>;
+            { items.map((item, index) => {
+                return <option key={ index } value={ item.name }>{ item.name }</option>;
             })}
         </select>
     )
--- components/index/educationHistory.js
+++ components/index/educationHistory.js
@@ -3,25 +3,21 @@ import ListContent from "./listContents";
 import { useState, useEffect } from "react";
 
 export default function EducationHistory() {
+    /** 変数定義 ---------------------------------------------------------------------*/
+    // ローカルストレージ構造定義
     const storageData = {
         inputs: [],
     }
-
+    // ステート
     const [inputs, setInputs] = useState([]);
+    // 入力セット構造定義
     const inputType = [
         {type: "date", id: "edu_date", displayName: "日付", cls: "w-full"},
         {type: "text", id: "school", displayName: "学校名", cls: "w-full"},
         {type: "text", id: "sch_in_out", displayName: "入学・卒業等", cls: "w-full"},
     ];
-
-    const handleBeforeunload = (e) => {
-        const items = {
-          inputs: inputs,
-        }
-        localStorage.setItem("educationHistory", JSON.stringify(items));
-        console.log("save", items);
-    };
-
+    /** 副作用フック -----------------------------------------------------------------*/
+    // ローカルストレージからデータの取得(初期値のセット)
     useEffect(() => {
         // ローカルストレージの内容を取得
         const jsonItems = localStorage.getItem("educationHistory");
@@ -35,12 +31,14 @@ export default function EducationHistory() {
           setInputs(storageData.inputs);
         }
     }, []);
-      
+    // ローカルストレージへデータの保存(ステートが変更されるごと)
     useEffect(() => {
-        window.addEventListener("beforeunload", handleBeforeunload);
-        return () => window.removeEventListener("beforeunload", handleBeforeunload);
+        const items = {
+            inputs: inputs,
+        }
+        localStorage.setItem("educationHistory", JSON.stringify(items));
     }, [inputs]);
-
+    /** コンポーネント返却 ------------------------------------------------------------*/
     return (
         <div className="p-2 md:p-6">
             <h2 className="text-yellow-500 text-lg text-center">- 学歴情報 -</h2>
